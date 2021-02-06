1cf80023798eb53bdd44f09160ee09e22609f2db
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-02-06 20:50:52 +0900

    user.rbにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   has_many :expenses
   has_many :incomes
 end
