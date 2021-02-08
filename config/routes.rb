c7a53eff7485a9f70faebe111ec3d63b6222a0f8
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-02-08 09:52:15 +0900

    支出と収入の詳細画面を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "expenses#index"
-  resources :expenses, only: [:new, :create, :show]
-  resources :incomes, only: [:new, :create, :show]
+  resources :expenses, only: [:new, :create, :show, :edit, :update]
+  resources :incomes, only: [:new, :create, :show, :edit, :update]
 end
