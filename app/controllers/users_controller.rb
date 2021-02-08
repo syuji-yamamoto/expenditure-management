5b88e33af8db37743ab755e3e936ca5945825819
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-02-08 17:41:25 +0900

    マイページで収支金額の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
 		@expense_sum_price = expenses.all.sum(:price)
 		incomes = current_user.incomes
 		@income_sum_price = incomes.all.sum(:price)
+		@total_amount = @income_sum_price - @expense_sum_price
 	end
 end
